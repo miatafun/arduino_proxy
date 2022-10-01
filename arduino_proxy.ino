53faab4637d370446966156c5bf17dee63f3f48f
Randy Almand <55414251+miatafun@users.noreply.github.com>
2022-10-01 15:12:00 -0700

    rename files (#7)

--- arduino_proxy.ino
+++ arduino_proxy.ino
@@ -15,7 +15,7 @@
 #include <WiFiNINA.h>
 #include <Ethernet.h>
 
-#include "arduino_secrets.h"
+#include "secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
